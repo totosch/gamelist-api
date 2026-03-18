c36278e6b1d9170a430790f5cf0422305696c456
totosch <totokennedy@gmail.com>
2026-03-18 10:03:22 -0300

    fix en swagger en prod

--- src/main/java/com/sch/gamelist_api/config/SwaggerConfig.java
+++ src/main/java/com/sch/gamelist_api/config/SwaggerConfig.java
@@ -2,10 +2,13 @@ package com.sch.gamelist_api.config;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @SecurityScheme(
@@ -23,6 +26,10 @@ public class SwaggerConfig {
                         .title("Gamelist API")
                         .description("REST API for managing a video game list")
                         .version("1.0.0")
-                );
+                )
+                .servers(List.of(
+                        new io.swagger.v3.oas.models.servers.Server().url("https://gamelist-api-production-7e74.up.railway.app").description("Production"),
+                        new io.swagger.v3.oas.models.servers.Server().url("http://localhost:8080").description("Local")
+                ));
     }
 }
