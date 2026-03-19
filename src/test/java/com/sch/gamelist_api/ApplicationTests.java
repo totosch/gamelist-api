eed576fa2c4d267508215c60eec3490fc2902459
totosch <totokennedy@gmail.com>
2026-03-18 21:51:13 -0300

    disabled applicationTest

--- src/test/java/com/sch/gamelist_api/ApplicationTests.java
+++ src/test/java/com/sch/gamelist_api/ApplicationTests.java
@@ -1,8 +1,10 @@
 package com.sch.gamelist_api;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class ApplicationTests {
 
